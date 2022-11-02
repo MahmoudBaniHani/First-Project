7afca24e6b495e920149aca686aa541328c4fd48
MahmoudBaniHani <mbanihani@everconnect>
2022-11-02 10:11:14 +0200

    ATM Exam update

--- ATM.rb
+++ ATM.rb
@@ -1,23 +1,22 @@
 password = "mahmoud"
-amount =1000
+amount = 1000
 count = 0
 while count <3 
- 	puts "Please enter your password"
+ 	puts "Please enter your password:"
  	pass = gets
  	result = (pass.strip() == password) ? true  : false
- 	if result
- 		puts "Please enter your amount :"
+ 	if result		
  		count2 = 0
  		while count2 < 3
+ 			puts "Please enter your amount :"
  			amount_in = gets.chop
  			if (amount_in.to_i <= amount and amount_in.to_i > 0)
- 				total_amount =(amount - amount_in.to_i)
- 				puts "Thank you tha amount is "+ amount_in +"  and total amount =" + total_amount.to_s
+ 				total_amount = (amount - amount_in.to_i)
+ 				puts "Thank you , the amount is out "+ amount_in +"  and total amount = " + total_amount.to_s
  				break
  			else
- 				puts "amount is less than number" + count2.to_s + " the rest of 3"
+ 				puts "the amout not valid :" + count2.to_s + " the rest of 3"
  				count2 +=1
-
  			end
  		end
  		break
